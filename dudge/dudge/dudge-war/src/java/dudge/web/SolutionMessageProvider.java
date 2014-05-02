cf9a66d3a1433457f7ccf09803c47210057f2470
Aleksandr Tsyganov <hitrolisk@gmail.com>
2014-05-02 21:59:06 +0400

    Фикс локализации статусов решений.

--- dudge/dudge/dudge-war/src/java/dudge/web/SolutionMessageProvider.java
+++ dudge/dudge/dudge-war/src/java/dudge/web/SolutionMessageProvider.java
@@ -9,9 +9,11 @@ package dudge.web;
 import dudge.db.RunResultType;
 import dudge.db.SolutionStatus;
 import dudge.solution.SolutionMessageSource;
+import java.util.Locale;
 import javax.servlet.http.HttpServletRequest;
 import org.apache.struts.Globals;
 import org.apache.struts.util.MessageResources;
+import org.apache.struts.util.RequestUtils;
 
 /**
  *
@@ -19,9 +21,11 @@ import org.apache.struts.util.MessageResources;
  */
 public class SolutionMessageProvider implements SolutionMessageSource {
 	MessageResources messageResources;
+	Locale locale;
 	
 	public SolutionMessageProvider(HttpServletRequest request) {
 		messageResources = (MessageResources) request.getAttribute(Globals.MESSAGES_KEY);
+		locale = RequestUtils.getUserLocale(request, null);
 	}
 	
 	public static SolutionMessageProvider getInstance(HttpServletRequest request) {
@@ -30,17 +34,17 @@ public class SolutionMessageProvider implements SolutionMessageSource {
 	
 	@Override
 	public String getSolutionStatusMessage(SolutionStatus status) {
-		return messageResources.getMessage("solution.status." + status.toString());
+		return messageResources.getMessage(locale, "solution.status." + status.toString());
 	}
 
 	@Override
 	public String getRunResultMessage(RunResultType runResult) {
-		return messageResources.getMessage("solution.status." + runResult.toString());
+		return messageResources.getMessage(locale, "solution.status." + runResult.toString());
 	}
 
 	@Override
 	public String getOnTestMessage() {
-		return messageResources.getMessage("solution.onTest");
+		return messageResources.getMessage(locale, "solution.onTest");
 	}
 	
 }
